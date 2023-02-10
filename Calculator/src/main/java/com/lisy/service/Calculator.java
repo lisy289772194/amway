9bd0e9b593cf4513667eb5d7db86a14a642e9ba9
lishuyang <lishuyang@joyy.com>
2023-02-10 16:57:29 +0800

    优化

--- Calculator/src/main/java/com/lisy/service/Calculator.java
+++ Calculator/src/main/java/com/lisy/service/Calculator.java
@@ -1,15 +1,28 @@
 package com.lisy.service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Calculator {
 
-	private int total=0;
+	private final AtomicLong total = new AtomicLong(0L);
 	public void operation(char operator,int operand) {
-		switch(operator) {
-		case '+':total+=operand;break;
-		case '-':total-=operand;break;
-		case '*':total*=operand;break;
-		case '/':total/=operand;break;
+		switch (operator) {
+
+			case '+':
+				total.addAndGet(operand);
+				break;
+			case '-':
+				total.addAndGet(-operand);
+				break;
+			case '*':
+				total.accumulateAndGet(operand, (left, right) -> left * right);
+				break;
+			case '/':
+				total.accumulateAndGet(operand, (left, right) -> left / right);
+				break;
+			default:
+				throw new RuntimeException("unexpected branch!");
 		}
-		System.out.println(operator+" "+operand+" ="+total);
+		System.out.println(operator+" "+operand+" ="+total.get());
 	}
 }
--- Calculator/src/main/java/com/lisy/service/Invoker.java
+++ Calculator/src/main/java/com/lisy/service/Invoker.java
@@ -4,20 +4,22 @@ import com.lisy.command.CalculatorCommand;
 import com.lisy.command.Command;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 public class Invoker {
 
-    private Calculator calculator = new Calculator();
-    private ArrayList<Command> commands = new ArrayList<Command>();
-    private int current = 0;
+    private final Calculator calculator = new Calculator();
+    private final ArrayList<Command> commands = new ArrayList<>();
+    private final AtomicLong current = new AtomicLong(0L);
+
 
     public void redo(int levels) {
         System.out.println("redo levels:" + levels);
         for (int i = 0; i < levels; i++) {
-            if (current < commands.size() - 1) {
+            if (current.get() < commands.size() - 1) {
                 commands.get(i).execute();
-                current++;
+                current.incrementAndGet();
             }
         }
     }
@@ -25,8 +27,8 @@ public class Invoker {
     public void undo(int levels) {
         System.out.println("undo levels:" + levels);
         for (int i = 0; i < levels; i++) {
-            if (current > 0) {
-                --current;
+            if (current.get() > 0) {
+                current.decrementAndGet();
                 commands.get(i).unexecute();
             }
         }
@@ -36,6 +38,6 @@ public class Invoker {
         Command command = new CalculatorCommand(calculator, operator, operand);
         command.execute();
         commands.add(command);
-        current++;
+        current.incrementAndGet();
     }
 }
